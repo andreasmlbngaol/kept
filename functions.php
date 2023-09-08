62ae39bea451039e52a930680efaa9c37a2d264e
andreasmlbngaol <lgandre45@gmail.com>
2023-09-09 00:12:57 +0700

    Mengubah pengirim email menjadi keptflow@gmail.com

--- functions.php
+++ functions.php
@@ -152,8 +152,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'lgandre45@gmail.com';                     //SMTP username
-$mail->Password   = 'hbdlnsvugxgvruhm';                               //SMTP password
+$mail->Username   = 'keptflow@gmail.com';                     //SMTP username
+$mail->Password   = 'ukgaqxlmcmdvyloa';                               //SMTP password
 $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
 $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
