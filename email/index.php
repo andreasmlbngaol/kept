5d972afd548a4df5d36aab5e5ce2f1b93ddeb2c5
andreasmlbngaol <lgandre45@gmail.com>
2023-09-09 00:20:42 +0700

    Modif kode

--- email/index.php
+++ email/index.php
@@ -1,7 +1,7 @@
 <?php
 require "../functions.php";
 $code = strval(100000,999999);
-if(sendEmail("$code", "andreaspremium006@gmail.com") == true) {
+if(sendEmail($code, "andreaspremium006@gmail.com") == true) {
     alert('Sukses');
     jumpTo('../index.php');
 }
